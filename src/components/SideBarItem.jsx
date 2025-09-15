ae1925b5ef3477e5a005b366a7e39518e3072637
Alejo-P <marcelo.pinzon@epn.edu.ec>
2025-09-14 21:20:52 -0500

    feat: mejorar el componente SideBarItem con animaciones y gestión de tooltip

--- src/components/SideBarItem.jsx
+++ src/components/SideBarItem.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
 import clsx from 'clsx';
 
 const SideBarItem = ({ icon: Icon, label, to = '', isDark, onClick, isSelected, isMobile = false }) => {
     const [isOpenTooltip, setIsOpenTooltip] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
     const handleClick = () => {
         onClick(to);
+    };
+
+    const afterShow = () => {
+        setTimeout(() => {
+            setIsOpenTooltip(false);
+        }, 5000);
     };
 
     const baseClasses =
@@ -14,14 +21,14 @@ const SideBarItem = ({ icon: Icon, label, to = '', isDark, onClick, isSelected, 
 
     const textClasses = clsx(
         {
-            "text-blue-400 border-b-2 border-blue-400": isSelected,
+            "text-blue-400 border-b-2 border-blue-400 font-bold": isSelected,
             "text-white cursor-pointer": isDark && !isSelected,
             "text-gray-900 cursor-pointer": !isDark && !isSelected,
         }
     );
 
     return (
-        <>
+        <AnimatePresence>
             <motion.div
                 className={clsx(baseClasses, textClasses)}
                 title={label}
@@ -30,21 +37,39 @@ const SideBarItem = ({ icon: Icon, label, to = '', isDark, onClick, isSelected, 
                 onClick={handleClick}
                 onMouseEnter={() => setIsOpenTooltip(true)}
                 onMouseLeave={() => setIsOpenTooltip(false)}
+                initial={{ width: isSelected ? 160 : 40 }}
+                animate={{ width: isHovered || isSelected ? 160 : 40 }}
+                onHoverStart={() => setIsHovered(true)}
+                onHoverEnd={() => setIsHovered(false)}
+                key={label}
                 layout
                 transition={{ type: "spring", stiffness: 300, damping: 20 }}
             >
-                <Icon className="w-5 h-5" />
-                {!isMobile && (
-                    <motion.span
-                        className={`hidden group-hover:block ml-2 whitespace-nowrap overflow-hidden text-ellipsis
-              ${isSelected ? 'text-blue-400 sm:block' : ''}`}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
+                <AnimatePresence>
+                    <motion.div
+                        className="flex justify-center items-center"
+                        initial={{ scale: 1 }}
+                        animate={{ scale: isHovered || isSelected ? 1.2 : 1 }}
+                        transition={{ type: "spring", stiffness: 300, damping: 20 }}
+                        key={label + "icon"}
+                        layout
                     >
-                        {label}
-                    </motion.span>
-                )}
+                        <Icon className="w-5 h-5" />
+                    </motion.div>
+                    {(!isMobile && (isHovered || isSelected)) && (
+                        <motion.span
+                            className={`group-hover:block ml-2 whitespace-nowrap overflow-hidden text-ellipsis ${isSelected ? 'text-blue-400' : ''}`}
+                            initial={{ opacity: 0, y: 10 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: -10 }}
+                            transition={{ duration: 0.25 }}
+                            key={label}
+                            layout
+                        >
+                            {label}
+                        </motion.span>
+                    )}
+                </AnimatePresence>
             </motion.div>
 
             {isMobile && (
@@ -54,9 +79,10 @@ const SideBarItem = ({ icon: Icon, label, to = '', isDark, onClick, isSelected, 
                     variant={isDark ? "dark" : "light"}
                     delayShow={100}
                     isOpen={isOpenTooltip}
+                    afterShow={afterShow}
                 />
             )}
-        </>
+        </AnimatePresence>
     );
 };
 
